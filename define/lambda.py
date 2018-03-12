2cbf76dcf7961b20be371b5bf08a5188eac92d82
Drew Minnear <drew.minnear@originate.com>
2018-03-12 17:29:24 -0400

    update /define lambda to v2

--- define/lambda.py
+++ define/lambda.py
@@ -1,24 +1,29 @@
 from xml.dom.minidom import parseString
-import boto3
 import requests
+import os
+import json
 
-s3_client = boto3.client('s3')
-
+# Set tokens from env vars
 try:
-  slackToken = s3_client.get_object(Bucket='dm-slack-secrets', Key='slack_token')['Body'].read().strip()
-  dictionaryAPIToken = s3_client.get_object(Bucket='dm-slack-secrets', Key='dictionary_api_token')['Body'].read().strip()
+  slackToken = os.environ['SLACK_TOKEN']
+  dictionaryAPIToken = os.environ['DICTIONARY_API_TOKEN']
 
 except Exception as e:
-  fail('Unable to get secret tokens from s3. Exception: ' + str(e) + '.')
+  fail('Environment not properly configured. Expected SLACK_TOKEN and DICTIONARY_API_TOKEN environment variables.')
 
 def handler(event, context):
   if slackToken != event['token']:
     fail('Invalid slack token provided.')
 
+  responseUrl = event['response_url']
+
   # Slack encodes spaces with a '+', but the dictionary api is not a fan.
   encoded = event['text'].replace('+', '%20')
 
   r = requests.get('http://www.dictionaryapi.com/api/v1/references/collegiate/xml/' + encoded, params={'key': dictionaryAPIToken})
+
+  if r.status_code != 200:
+    fail("Request to dictionary api failed.")
 
   dom = parseString(r.text.strip())
 
@@ -31,7 +36,11 @@ def handler(event, context):
     entry =  entry_list.firstChild
     while entry != None:
       if entry.nodeType == entry.ELEMENT_NODE and entry.tagName == 'entry' and entry.getAttribute('id'):
-        return processEntry(entry)
+        responseBody = processEntry(entry)
+        r = requests.post(responseUrl, data=responseBody)
+        if r.status_code != 200:
+          fail("Request to slack webhook failed. Response: ", r.text)
+        return ''
 
       entry = entry.nextSibling
 
@@ -40,17 +49,21 @@ def handler(event, context):
   except Exception as e:
     fail('Unexpected exception processing XML. Exception: ' + str(e) + '.', dom)
 
+# Log to cloudwatch and except out of lambda
 def fail(err_str, dom = None):
   if dom != None:
     print 'XML: ' + dom.toxml() + '.'
 
   raise Exception(err_str)
 
+# Process dictionary api entry into json object to be sent to slack
 def processEntry(entry):
   dts = entry.getElementsByTagName('dt')
   definitions = [dtToString(dt) for dt in dts]
-  return [{'color': '3C0857', 'text': definition} for definition in definitions]
+  attachments = [{'color': '3C0857', 'text': definition} for definition in definitions]
+  return json.dumps({'attachments': attachments, 'response_type': 'ephemeral'})
 
+# Sometimes definitions are nested inside other xml objects
 def dtToString(dt):
   dt_str = ''
 
